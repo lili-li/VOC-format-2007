8072a1714c8e8967733357599aab2241e643642a
lili-li <905572538@qq.com>
2020-05-31 11:54:06 +0800

    create main doc's four xxx.txts

---
+++ 04-create_txt.py
@@ -0,0 +1,38 @@
+#4. 生成main文件中的4个txt文件
+
+import os
+import random
+
+trainval_percent = 0.625
+train_percent = 0.375
+xmlfilepath = 'C:\Software\pycharm_office\safe_hat\VOCdevkit\VOC2007\Annotations'
+txtsavepath = 'C:\Software\pycharm_office\safe_hat\VOCdevkit\VOC2007\ImageSets\Main'
+total_xml = os.listdir(xmlfilepath)
+
+num = len(total_xml)
+list = range(num)
+tv = int(num * trainval_percent)
+tr = int(tv * train_percent)
+trainval = random.sample(list, tv)
+train = random.sample(trainval, tr)
+
+ftrainval = open(txtsavepath + '/trainval.txt', 'w')
+ftest = open(txtsavepath + '/test.txt', 'w')
+ftrain = open(txtsavepath + '/train.txt', 'w')
+fval = open(txtsavepath + '/val.txt', 'w')
+
+for i in list:
+    name = total_xml[i][:-4] + '\n'
+    if i in trainval:
+        ftrainval.write(name)
+        if i in train:
+            ftrain.write(name)
+        else:
+            fval.write(name)
+    else:
+        ftest.write(name)
+
+ftrainval.close()
+ftrain.close()
+fval.close()
+
